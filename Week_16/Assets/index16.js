3f8d55d9cbab109e6ceea7a7cdf572b2723d8c55
Veronika Chupova <della85c@yahoo.com>
2022-12-18 14:37:12 +0100

    hw queryselector

---
+++ Week_16/Assets/index16.js
@@ -0,0 +1,22 @@
+/*function sumInput() {
+    let numbers = document.getElementsByName('input')[0].value;
+    let sorted = sort (numbers);
+    
+    let sum1 = Number(firstNumber) + Number(secondNumber);
+    
+    document.getElementsByName('arraysorted')[0].value= sorted;
+    
+    }*/
+
+    function sumInput() {
+        let numbers = new Array();
+
+        while (repeat !== "n")
+        {
+            let num = document.getElementsByName('input')[0].value;
+            let num.push(num);
+            
+        }
+    }
+
+    
