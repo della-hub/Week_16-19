3c76428fd38b860b192aba7ca6e97553ad3a95cb
Veronika Chupova <della85c@yahoo.com>
2023-01-22 17:00:48 +0100

    finished hw 17_1

--- Week_17/Assets/hw17_1.js
+++ Week_17/Assets/hw17_1.js
@@ -25,10 +25,25 @@ function sortAll() {
 } 
 }*/
 function addRecord() {
-    
-    let names = document.getElementById('namefull').value;
-    let namesarray = names.split(" ");
-alert (namesarray);
+    let name = document.getElementById('namefull').value;
+    name = name.trim();
+    name = name.replace(/,/g, '')
+    name = name.replace(/ +(?= )/g,'');
+    name = name.toLowerCase();
+    let names = name.split(" ");
+
+    for (let i = 0; i < names.length; i++) {
+        names[i] = names[i].charAt(0).toUpperCase() + names[i].slice(1);
+        console.log(names);
+    }
+    let output = document.getElementById('resultsoutput');
+    output.innerHTML = output.innerHTML + render(names);
 }
 
-let addButton = document.getElementById('input').value;
+function render (name) {
+    return "Name: " +  name[0] + "<br>" + "Middle Name: " + name[1] + "<br>" + "Surname: " + name[2]  +  "<br>";
+} 
+
+
+
+// let addButton = document.getElementById('input').value;
