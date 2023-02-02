2db188c0d5e64004c4259a57c62fb2c7a8ce5aec
Veronika Chupova <della85c@yahoo.com>
2023-02-02 21:07:41 +0100

    hw17-4 complete

--- Week_17/Assets/hw17_4.js
+++ Week_17/Assets/hw17_4.js
@@ -1,18 +1,28 @@
 let Randomnumbers = Array(10).fill(0);
 
 function generateRandomNumbers() {
+    clear('array'); 
     Randomnumbers = Randomnumbers.map(
         () => { return Number(Math.round((Math.random()*(10 - (-10)) + (-10)))) }
     );
+    printResult('Array', Randomnumbers, "array")
 }
 
 function calculate() {
-    let max = Math.max(...Randomnumbers);
-    let min = Math.min(...Randomnumbers);
+    clear('output')
+    printResult('Max number', Math.max(...Randomnumbers));
+    printResult('Min number', Math.min(...Randomnumbers));
+    printResult('Multi', Randomnumbers.reduce((total, num) => { return total * num }, 1));
     let sum = Randomnumbers.reduce((total, num) => { return total + num }, 0);
-    let multi = Randomnumbers.reduce((total, num) => { return total * num }, 1);
-    let avg = sum / Randomnumbers.length;
+    printResult('Sum', sum);
+    printResult('Average', sum / Randomnumbers.length);
+}
 
-    let randomOutput = document.getElementById('output');
-    randomOutput.innerHTML = randomOutput.innerHTML + `Array ${Randomnumbers}<br>Max number ${max} <br>Min number ${min} <br>Sum of all values ${sum} <br>Average of all values ${avg} <br>Multiplication of all values ${multi}`;
+function printResult(descr, result, field="output") {
+    let randomOutput = document.getElementById(field);
+    randomOutput.innerHTML = randomOutput.innerHTML + `${descr} ${result}<br>`;
+}
+
+function clear(field) {
+    document.getElementById(field).innerHTML = "";
 }
