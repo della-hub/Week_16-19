9c824433347d2003c209dec2b0c81283ce542725
Veronika Chupova <della85c@yahoo.com>
2023-02-06 08:12:43 +0100

    added sD formula

--- Week_16/Assets/calculator.js
+++ Week_16/Assets/calculator.js
@@ -1,23 +1,49 @@
+"use strict";
+
+Array.from(document.getElementsByTagName("input")).forEach(function(item) {
+   item.addEventListener("change", calculateTotal);
+});
+
 function saveData() {
-    let record = document.querySelector('#inputtext').value;
+    
+    let volume = document.querySelector('#volume').value;
+    let age = document.querySelector('#age').value;
+    let transmission = document.querySelector('#transmission').value;
+    let secondhand = document.querySelector('input[name=sh]:checked').value;
+    let enginetype = document.querySelector('input[name=eng]:checked').value;
 
-    if (!record) {
-        return;
-    }
+    let cost = document.getElementById("cost");
+    cost.innerHTML = cost.innerHTML + volume + age + transmission + secondhand + enginetype;
 
-    records.push(record);
-    document.querySelector('#inputtext').value = '';
+    // $('input, select').each(function() {
+    //     if($(this).val() == ''){
+    //         alert('Cannot be blank!');
+    //     }
+    //     else{
+    //        alert('Being calculated..')
+           
+    //     }
+    // })
+
+    // let record = document.querySelector('#inputtext').value;
+    // if (!record) {
+    //     return;
+    // }
+
+    // records.push(record);
+    // document.querySelector('#inputtext').value = '';
 }
 
 function calculateTotal() {
-    var total = 0;//
-    var list = document.getElementsByClassName("inputtext");
-    var values = [];
-    for(var i = 0; i < list.length; ++i) {
-        values.push(parseFloat(list[i].value));
-    }
-    total = values.reduce(function(previousValue, currentValue, index, array){
-        return previousValue + currentValue;
-    });
-    document.getElementById("total").value = total;    
+    console.log('28');
+    // var total = 0;
+    // var list = document.getElementsByClassName("inputtext");
+    // var values = [];
+    // for(var i = 0; i < list.length; ++i) {
+    //     values.push(parseFloat(list[i].value));
+    // }
+    // total = values.reduce(function(previousValue, currentValue, index, array){
+    //     return previousValue + currentValue;
+    // });
+    // document.getElementById("total").value = total;    
 }
